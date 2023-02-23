d5236c1effe29d0d66b22d8f63f4b881285dfdc2
Jake Soenneker <jake@soenneker.com>
2023-02-22 20:34:59 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.Dictionary.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Dictionary/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.dictionary/main.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.dictionary/actions/workflows/main.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.Dictionary.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Dictionary/)
+
+# Soenneker.Extensions.Dictionary
+### A collection of helpful Dictionary extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.Dictionary
+```
---
+++ src/DictionaryExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace Soenneker.Extensions.Dictionary;
+
+/// <summary>
+/// A collection of helpful Dictionary extension methods
+/// </summary>
+public static class DictionaryExtension
+{
+    /// <summary>
+    /// Flattens all of the values in a dictionary and returns a new list with all of them
+    /// </summary>
+    [Pure]
+    public static List<TValue> ToFlattenedValuesList<TKey, TValue>(this IDictionary<TKey, IList<TValue>> value) where TKey : notnull
+    {
+        List<TValue> result = value.SelectMany(item => item.Value).ToList();
+        return result;
+    }
+
+    /// <summary>
+    /// Loops over the target and adds each of the items into the source. Useful for readonly scenarios.
+    /// </summary>
+    public static void AddDictionary<TKey, TValue>(this IDictionary<TKey, TValue> value, IDictionary<TKey, TValue> dictionary)
+    {
+        foreach (KeyValuePair<TKey, TValue> kvp in dictionary)
+        {
+            value.Add(kvp);
+        }
+    }
+}
