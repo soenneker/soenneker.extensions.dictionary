f5c83898eb7c7dbbbe7e1afec2d3eadbfe85c32a
Jake Soenneker <jake@soenneker.com>
2025-01-14 11:30:30 -0600

    Improvements for perf and allocation

--- src/DictionaryExtension.cs
+++ src/DictionaryExtension.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -12,11 +11,44 @@ namespace Soenneker.Extensions.Dictionary;
 /// </summary>
 public static class DictionaryExtension
 {
-    /// <inheritdoc cref="ToFlattenedValuesList{TKey,TValue}(IDictionary{TKey,List{TValue}})"/>
+    /// <summary>
+    /// Flattens the values of a dictionary, where each key maps to a list of values, into a single list.
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+    /// <param name="dictionary">The dictionary to flatten.</param>
+    /// <returns>A single list containing all values from the dictionary.</returns>
     [Pure]
     public static List<TValue> ToFlattenedValuesList<TKey, TValue>(this IDictionary<TKey, IList<TValue>> dictionary) where TKey : notnull
     {
-        List<TValue> result = dictionary.SelectMany(item => item.Value).ToList();
+        // Pre-calculate the total capacity for the resulting list
+        var totalCapacity = 0;
+        using IEnumerator<KeyValuePair<TKey, IList<TValue>>> enumerator = dictionary.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            KeyValuePair<TKey, IList<TValue>> kvp = enumerator.Current;
+            if (kvp.Value is not null)
+                totalCapacity += kvp.Value.Count;
+        }
+
+        // Create the result list with the pre-calculated capacity
+        var result = new List<TValue>(totalCapacity);
+
+        // Populate the result list with values from the dictionary
+        using IEnumerator<KeyValuePair<TKey, IList<TValue>>> valueEnumerator = dictionary.GetEnumerator();
+        while (valueEnumerator.MoveNext())
+        {
+            KeyValuePair<TKey, IList<TValue>> kvp = valueEnumerator.Current;
+            if (kvp.Value is not null)
+            {
+                // Add items manually to avoid calling AddRange (avoids potential overhead in large datasets)
+                for (var i = 0; i < kvp.Value.Count; i++)
+                {
+                    result.Add(kvp.Value[i]);
+                }
+            }
+        }
+
         return result;
     }
 
@@ -26,9 +58,34 @@ public static class DictionaryExtension
     [Pure]
     public static List<TValue> ToFlattenedValuesList<TKey, TValue>(this IDictionary<TKey, List<TValue>> dictionary) where TKey : notnull
     {
-        List<TValue> result = dictionary.SelectMany(item => item.Value).ToList();
+        // Pre-calculate total capacity for the resulting list
+        var totalCapacity = 0;
+        using IEnumerator<KeyValuePair<TKey, List<TValue>>> enumerator = dictionary.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            KeyValuePair<TKey, List<TValue>> kvp = enumerator.Current;
+            if (kvp.Value is not null)
+                totalCapacity += kvp.Value.Count;
+        }
+
+        // Create the result list with the pre-calculated capacity
+        var result = new List<TValue>(totalCapacity);
+
+        // Populate the result list with values from the dictionary
+        using IEnumerator<KeyValuePair<TKey, List<TValue>>> valueEnumerator = dictionary.GetEnumerator();
+        while (valueEnumerator.MoveNext())
+        {
+            KeyValuePair<TKey, List<TValue>> kvp = valueEnumerator.Current;
+            if (kvp.Value is not null)
+            {
+                // Use AddRange to minimize per-item calls
+                result.AddRange(kvp.Value);
+            }
+        }
+
         return result;
     }
+
 
     /// <summary>
     /// Adds (or updates!) an enumerable to a dictionary without a loop in managed code. <para/>
@@ -39,11 +96,15 @@ public static class DictionaryExtension
     /// <exception cref="NotSupportedException"></exception>
     public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> source, IEnumerable<TValue> toAdd, Expression<Func<TValue, TKey>> selector)
     {
-        Func<TValue, TKey> compiled = selector.Compile();
+        Func<TValue, TKey> keySelector = selector.Compile();
 
-        foreach (TValue item in toAdd)
+        using IEnumerator<TValue> enumerator = toAdd.GetEnumerator();
+        while (enumerator.MoveNext())
         {
-            TKey? key = compiled(item);
+            TValue item = enumerator.Current;
+
+            // Compute the key and add/update the dictionary
+            TKey key = keySelector(item);
             source[key] = item;
         }
     }
@@ -54,9 +115,11 @@ public static class DictionaryExtension
     /// </summary>
     public static void AddDictionary<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> dictionary)
     {
-        foreach (KeyValuePair<TKey, TValue> kvp in dictionary)
+        using IEnumerator<KeyValuePair<TKey, TValue>> enumerator = dictionary.GetEnumerator();
+        while (enumerator.MoveNext())
         {
-            source.Add(kvp);
+            KeyValuePair<TKey, TValue> kvp = enumerator.Current;
+            source[kvp.Key] = kvp.Value; // Adds or updates the key-value pair
         }
     }
 
@@ -66,34 +129,84 @@ public static class DictionaryExtension
     [Pure]
     public static T ToObject<T>(this IDictionary<string, object> source) where T : class, new()
     {
+        // Create an instance of the target type
         var someObject = new T();
-        Type someObjectType = someObject.GetType();
+        Type someObjectType = typeof(T);
 
-        foreach (KeyValuePair<string, object> item in source)
+        // Cache the properties for the type
+        var properties = new Dictionary<string, PropertyInfo>(StringComparer.OrdinalIgnoreCase);
+        foreach (PropertyInfo property in someObjectType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
         {
-            PropertyInfo? property = someObjectType.GetProperty(item.Key);
+            if (property.CanWrite) // Only consider writable properties
+            {
+                properties[property.Name] = property;
+            }
+        }
 
-            if (property != null)
-                property.SetValue(someObject, item.Value, null);
+        // Iterate through the dictionary using an enumerator
+        using IEnumerator<KeyValuePair<string, object>> enumerator = source.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            KeyValuePair<string, object> item = enumerator.Current;
+
+            if (!properties.TryGetValue(item.Key, out PropertyInfo? property))
+                continue;
+
+            // Attempt to set the value if types match or can be converted
+            if (item.Value is null || property.PropertyType.IsInstanceOfType(item.Value))
+            {
+                property.SetValue(someObject, item.Value);
+            }
+            else if (TryConvertValue(item.Value, property.PropertyType, out object? convertedValue))
+            {
+                property.SetValue(someObject, convertedValue);
+            }
         }
 
         return someObject;
     }
 
     /// <summary>
+    /// Attempts to convert a value to the target type.
+    /// </summary>
+    private static bool TryConvertValue(object value, Type targetType, out object? convertedValue)
+    {
+        try
+        {
+            convertedValue = Convert.ChangeType(value, targetType);
+            return true;
+        }
+        catch
+        {
+            convertedValue = null;
+            return false;
+        }
+    }
+
+    /// <summary>
     /// Tries to retrieve a key from a particular value in the dictionary. If there are multiple of the same value, it returns the first key.
     /// </summary>
     [Pure]
-    public static bool TryGetKeyFromValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TValue value, out TKey? result) where TValue : class
+    public static bool TryGetKeyFromValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TValue value, out TKey? result) where TValue : class
     {
-        KeyValuePair<TKey, TValue>? kvp = dictionary.FirstOrDefault(x => x.Value == value);
+        // Get the enumerator for the dictionary
+        using IEnumerator<KeyValuePair<TKey, TValue>> enumerator = dictionary.GetEnumerator();
 
-        if (kvp != null)
+        // Iterate using the enumerator
+        while (enumerator.MoveNext())
         {
-            result = kvp.Value.Key;
-            return true;
+            KeyValuePair<TKey, TValue> kvp = enumerator.Current;
+
+            // Check for matching value using reference equality first, then value equality
+            if (ReferenceEquals(kvp.Value, value) || (kvp.Value?.Equals(value) ?? false))
+            {
+                result = kvp.Key;
+                return true;
+            }
         }
 
+        // If no match is found, set result to default
         result = default;
         return false;
     }
--- test/Soenneker.Extensions.Dictionary.Tests/ComplexObject.cs
+++ test/Soenneker.Extensions.Dictionary.Tests/ComplexObject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Soenneker.Extensions.Dictionary.Tests
+﻿namespace Soenneker.Extensions.Dictionary.Tests
 {
     public class ComplexObject
     {
